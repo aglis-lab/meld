695f11437079920a5cafe05fff9af5e26cbe8bc7
zaen <gooner0709@gmail.com>
2026-08-02 23:53:26 +0700

    feat: add hello.tsx

---
+++ plugins/vite/src/components/Hello.tsx
@@ -0,0 +1,10 @@
+export function Hello() {
+  return (
+    <div>
+      <h1>Hello Vite + React!</h1>
+      <p>
+        Edit <code>src/components/Hello.tsx</code> and save to test HMR updates.
+      </p>
+    </div>
+  );
+}
